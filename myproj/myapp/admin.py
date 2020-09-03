89c8e155f119df3963ff008ed30b5ddf9ccad6eb
Yusuke MURAOKA <yusuke@jbking.org>
2020-09-03 19:46:21 +0900

    introduce token model

--- myproj/myapp/admin.py
+++ myproj/myapp/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from django.utils.html import format_html
+from myapp.models import Token
 
-# Register your models here.
+
+class TokenAdmin(admin.ModelAdmin):
+    list_display = ('data', 'enable', 'send_to_token')
+
+    def send_to_token(self, obj):
+        # Step 1 show button navigate to send text view
+        return format_html(
+            '<form method="post" target=""><input type="submit"/></form>'
+        )
+
+    send_to_token.allow_tags = True
+
+    
+admin.site.register(Token, TokenAdmin)
